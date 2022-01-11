acbdf1893f1e0577bef3e8238b6b9b1eb9ca22ee
gfleuri <gedeonfleurissaint@gmail.com>
2022-01-11 12:07:26 -0500

    changing width

--- src/trends.js
+++ src/trends.js
@@ -348,10 +348,8 @@ export default function Trends() {
               >
                 <div
                   style={{
-                    display: "flex",
-                    justifyContent: "flex-start",
                     width: "800%",
-                    maxWidth: "365px",
+                    maxWidth: "375px",
                   }}
                 >
                   <button
