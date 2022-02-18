be8a0d9f7d8688d5b359e40eaf32e6f4bdb94c89
gfleuri <gedeonfleurissaint@gmail.com>
2022-02-18 13:38:33 -0500

    trend menu position

--- src/scroll.js
+++ src/scroll.js
@@ -32,19 +32,27 @@ export default function Scroll(props) {
           document.getElementById("backToTop").style.display = "none";
         }
 
+        // trend menu DOM
+        const trendMenu = document.getElementById("trendMenu");
+        const trendLoc = document.getElementById("trendLoc");
+
         // Fix trend menu to top of screen
-        if (document.getElementById("trendMenu") != null) {
+        if (trendMenu != null) {
           if (window.pageYOffset > 154) {
-            document.getElementById("trendMenu").style.position = "fixed";
-            document.getElementById("trendLoc").style.marginBottom = "84px";
-            document.getElementById("trendMenu").style.borderBottom =
-              props.theme === "light"
-                ? "1px solid rgb(0, 0, 0, 0.1)"
-                : "1px solid #222222";
+            if (trendMenu.style.position !== "fixed") {
+              trendMenu.style.position = "fixed";
+              trendLoc.style.marginBottom = "84px";
+              trendMenu.style.borderBottom =
+                props.theme === "light"
+                  ? "1px solid rgb(0, 0, 0, 0.1)"
+                  : "1px solid #222222";
+            }
           } else {
-            document.getElementById("trendMenu").style.position = "static";
-            document.getElementById("trendLoc").style.marginBottom = "0px";
-            document.getElementById("trendMenu").style.borderBottom = "none";
+            if (trendMenu.style.position !== "static") {
+              trendMenu.style.position = "static";
+              trendLoc.style.marginBottom = "0px";
+              trendMenu.style.borderBottom = "none";
+            }
           }
         }
       };
